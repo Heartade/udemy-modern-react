3ae01516b608ea1b45edf4d535b352208c5392ba
Soohan Park <soohan@swink.co.kr>
2021-03-29 18:32:38 +0900

    useEffect practice wip

--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,7 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const HooksContainer1 = props => {
     const [stateValue, setValue] = useState(0);
+    const [useEffectValue, setUseEffectValue] = useState(null);
+
+    useEffect(()=>{
+        setTimeout(()=>setUseEffectValue("useEffect worked"), 3000);
+    }, [stateValue])
 
     const increment = ()=>{
         setValue(stateValue+1);
@@ -10,12 +15,26 @@ const HooksContainer1 = props => {
         setValue(stateValue-1);
     }
 
+    const changeEffect = ()=>{
+        setUseEffectValue("useEffect by user");
+    }
+
     return (
     <div>
     Hooks
 
     <button onClick={increment}>{stateValue}+1</button>
     <button onClick={decrement}>{stateValue}-1</button>
+    <button onClick={changeEffect}>change useEffect value</button>
+    <div>
+        <p>
+            {
+                useEffectValue
+                ? <p>{useEffectValue}</p>
+                : <p>No Value</p>
+            }
+        </p>
+    </div>
     {console.log(props)}
     </div>
 )};
