21ce026a25d7640f6858118ade38bd2cf9a6e7e4
Soohan Park <soohan@swink.co.kr>
2021-03-30 19:34:16 +0900

    useDispatch() practice

--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,8 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useReducer} from 'react'
+import * as Reducer from '../store/hooks/hooks_reducer'
+import * as ACTIONS from '../store/actions/actions'
 
 const HooksContainer1 = props => {
     const [stateValue, setValue] = useState(0);
     const [useEffectValue, setUseEffectValue] = useState(null);
+    const [state, dispatch] = useReducer(Reducer.HooksReducer, Reducer.initialState)
 
     useEffect(()=>{
         setTimeout(()=>setUseEffectValue("useEffect worked"), 3000);
@@ -19,10 +22,23 @@ const HooksContainer1 = props => {
         setUseEffectValue("useEffect by user");
     }
 
+    const handleDispatchTrue = ()=>{
+        // dispatch(ACTIONS.SUCCESS); SAME
+        // dispatch({type: ACTIONS.SUCCESS}); SAME
+        dispatch(ACTIONS.success());
+    }
+    const handleDispatchFalse = ()=>{
+        // dispatch(ACTIONS.FAILURE); SAME
+        // dispatch({type: ACTIONS.FAILURE}); SAME
+        dispatch(ACTIONS.failure());
+    }
+
     return (
     <div>
     Hooks
 
+    <button onClick={handleDispatchTrue}>Dispatch True</button>
+    <button onClick={handleDispatchFalse}>Dispatch False</button>
     <button onClick={increment}>{stateValue}+1</button>
     <button onClick={decrement}>{stateValue}-1</button>
     <button onClick={changeEffect}>change useEffect value</button>
@@ -32,6 +48,14 @@ const HooksContainer1 = props => {
                 useEffectValue
                 ? <p>{useEffectValue}</p>
                 : <p>No Value</p>
+            }
+        </p>
+        <p>
+            StateProp1:
+            {
+                state.stateprop1
+                ? "True"
+                : "False"
             }
         </p>
     </div>
