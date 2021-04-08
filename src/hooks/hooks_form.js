d203c1fe060a06c272c748c83b32738a395b03df
Heartade <soobak0313@github.com>
2021-04-08 19:23:32 +0900

    form page implementation WIP

--- src/hooks/hooks_form.js
+++ src/hooks/hooks_form.js
@@ -1,14 +1,29 @@
 import React, {useState, useReducer, useContext} from 'react';
 
 const HooksForm = ()=>{
-    const [stateForm, setStateForm] = useState('')
+    const [valueChange, setValueChange] = useState('')
+    const [valueSubmit, setValueSubmit] = useState('')
+
+    const handleUseStateChange = (ev)=>{
+        setValueChange(ev.target.value);
+    }
+    const handleUseStateSubmit = (ev)=>{
+        ev.preventDefault();
+        setValueChange(ev.target.value);
+    }
 
     return (
         <div>
             <form>
                 <label> React useState: </label>
-                <input id='useState' type='text' onChange={()=>{}}></input>
+                <input id='useState' type='text' onChange={handleUseStateChange}></input>
+                <button type='submit'> Submit </button>
             </form>
+            <div>
+                <h2>  React useState: </h2>
+                <p> Change: {valueChange} </p>
+                <p> Submit: {valueSubmit} </p>
+            </div>
         </div>
     )
 }
